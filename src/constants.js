1265c4712fc2fde0fa6e0047a2f646cf41c3ed62
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-02-18 12:18:27 -0500

    added a few colors

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const WHITE = "#FEFFF";
+export const OFF_GRAY = "#DEF2F1";
+export const GRAY = "#819499";
+export const TEAL = "#3AAFA9";
+export const DARKER_TEAL = "#2B7A78";
+export const DARK_BLUE = "#1725A";
+export const DEFAULT_TEXT_COLOR = "#27363B";

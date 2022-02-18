03cb6a45976fc5f8f01bb01ef7c777c4979fc288
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-02-18 12:11:25 -0500

    Create custom theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import ContentLayout from "./contentLayout";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { GRAY, TEAL } from "./constants";
 
 function App() {
+  const theme = createTheme({
+    palette: { primary: { main: GRAY }, secondary: { main: TEAL } },
+    typography: {
+      h1: {
+        fontFamily: "Poppins",
+      },
+      h2: {
+        fontFamily: "Poppins",
+      },
+      h3: {
+        fontFamily: "Poppins",
+      },
+      h4: {
+        fontFamily: "Poppins",
+      },
+      h5: {
+        fontFamily: "Poppins",
+      },
+      h6: {
+        fontFamily: "Poppins",
+      },
+      p: {
+        fontFamily: "Poppins",
+        fontSize: "16px",
+      },
+      subtitle1: {
+        fontFamily: "Poppins",
+      },
+      subtitle2: {
+        fontFamily: "Poppins",
+      },
+      body1: {
+        fontFamily: "Poppins",
+      },
+      body2: {
+        fontFamily: "Poppins",
+      },
+      button: {
+        fontFamily: "Poppins",
+      },
+      caption: {
+        fontFamily: "Poppins",
+      },
+      overline: {
+        fontFamily: "Poppins",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ContentLayout />
+    </ThemeProvider>
   );
 }
 

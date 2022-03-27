5fdfa0674761667f646451d0d5291b407cee6f77
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-03-27 12:47:16 -0400

    Add colors to theme

---
+++ src/theme/contants.js
@@ -0,0 +1,2 @@
+export const PRIMARY_COLOR = "#FFFFFF";
+export const SECONDARY_COLOR = "#01b0d3";
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,48 +1,63 @@
+import { PRIMARY_COLOR, SECONDARY_COLOR } from "./contants";
+
 export const theme = {
-  //palette: { primary: { main: GRAY }, secondary: { main: TEAL } },
+  palette: { primary: { main: PRIMARY_COLOR }, secondary: { main: SECONDARY_COLOR } },
   typography: {
     h1: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     h2: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     h3: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     h4: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     h5: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     h6: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     p: {
       fontFamily: "Poppins",
       fontSize: "16px",
+      color: "#FFFFFF",
     },
     subtitle1: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     subtitle2: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     body1: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     body2: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     button: {
       fontFamily: "Poppins",
     },
     caption: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
     overline: {
       fontFamily: "Poppins",
+      color: "#FFFFFF",
     },
   },
   overrides: {

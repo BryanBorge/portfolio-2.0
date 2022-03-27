9f998630e5c8d15fd8cf656be20ef5401118c004
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-03-27 10:46:52 -0400

    Starter component layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "./Components/Navigation/Header/Header";
+import AboutMe from "./Pages/About";
+import Experience from "./Pages/Experience";
+import Landing from "./Pages/Landing";
+import { theme } from "./theme/theme";
 
 function App() {
+  const customTheme = createTheme(theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <Header />
+      <div
+        style={{
+          height: "100vh",
+          minHeight: "-webkit-fill-available",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Landing />
+      </div>
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          minHeight: "-webkit-fill-available",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <AboutMe />
+      </div>
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Experience />
+      </div>
+    </ThemeProvider>
   );
 }
 

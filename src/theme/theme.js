b61fd20602c0361ae9abd1806ead0eb461bad6a3
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-03-27 10:47:03 -0400

    custom theme

---
+++ src/theme/theme.js
@@ -0,0 +1,59 @@
+export const theme = {
+  //palette: { primary: { main: GRAY }, secondary: { main: TEAL } },
+  typography: {
+    h1: {
+      fontFamily: "Poppins",
+    },
+    h2: {
+      fontFamily: "Poppins",
+    },
+    h3: {
+      fontFamily: "Poppins",
+    },
+    h4: {
+      fontFamily: "Poppins",
+    },
+    h5: {
+      fontFamily: "Poppins",
+    },
+    h6: {
+      fontFamily: "Poppins",
+    },
+    p: {
+      fontFamily: "Poppins",
+      fontSize: "16px",
+    },
+    subtitle1: {
+      fontFamily: "Poppins",
+    },
+    subtitle2: {
+      fontFamily: "Poppins",
+    },
+    body1: {
+      fontFamily: "Poppins",
+    },
+    body2: {
+      fontFamily: "Poppins",
+    },
+    button: {
+      fontFamily: "Poppins",
+    },
+    caption: {
+      fontFamily: "Poppins",
+    },
+    overline: {
+      fontFamily: "Poppins",
+    },
+  },
+  overrides: {
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          "&.MuiButtonBase-root.MuiTab-root": {
+            fontSize: "20px",
+          },
+        },
+      },
+    },
+  },
+};

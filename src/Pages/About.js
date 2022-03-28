1c05b2be6b16b27a4a6996b31ccafedceaf92d8a
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-03-27 23:22:59 -0400

    basic/starter animations

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,7 @@ import Grid from "@mui/material/Grid";
 import { List, Typography } from "@mui/material";
 import CustomListItem from "../Components/CustomListItem/CustomListItem";
 import { PRIMARY_COLOR, SECONDARY_COLOR } from "../theme/contants";
+import { motion } from "framer-motion";
 const AboutMe = () => {
   return (
     <Box
@@ -14,69 +15,93 @@ const AboutMe = () => {
       style={{ width: "100%" }}
       sx={{ ml: 5, mr: 5, mb: 20 }}>
       <div style={{ maxWidth: "700px", display: "flex", flexDirection: "column", marginBottom: "20px" }}>
-        <Typography gutterBottom variant="p">
-          Although I gratuated from{" "}
-          <a href="https://www.farmingdale.edu/" target="_blank" rel="noopener">
-            SUNY Farmingdale
-          </a>{" "}
-          with a B.S. in Computer Programming and Information Systems I am a self-taught web developer
-          specializing in React.
-        </Typography>
-        <br />
-        <Typography gutterBottom variant="p">
-          I have had the opportunity to lead the development of a mobile application for an{" "}
-          <a href="https://www.manickatrecords.com/" target="_blank" rel="noopener">
-            independent record label
-          </a>
-          . Currently I'm working at{" "}
-          <a href="https://www.avi.com/" target="_blank" rel="noopener">
-            Applied Visions{" "}
-          </a>
-          as a Front End Developer building responsive and{" "}
-          <a href="https://mealtrack.com/" target="_blank" rel="noopener">
-            highly configurable software
-          </a>{" "}
-          for a variety of clients.
-        </Typography>
-        <br />
-        <Typography gutterBottom variant="p">
-          Here are some technologies I work with everyday:{" "}
-        </Typography>
+        <motion.div
+          style={{ display: "flex", flexDirection: "column" }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          initial={{ opacity: 0 }}>
+          <Typography gutterBottom variant="p">
+            Although I gratuated from{" "}
+            <a href="https://www.farmingdale.edu/" target="_blank" rel="noopener">
+              SUNY Farmingdale
+            </a>{" "}
+            with a B.S. in Computer Programming and Information Systems I am a self-taught web developer
+            specializing in React.
+          </Typography>
+          <br />
+          <Typography gutterBottom variant="p">
+            I have had the opportunity to lead the development of a mobile application for an{" "}
+            <a href="https://www.manickatrecords.com/" target="_blank" rel="noopener">
+              independent record label
+            </a>
+            . Currently I'm working at{" "}
+            <a href="https://www.avi.com/" target="_blank" rel="noopener">
+              Applied Visions{" "}
+            </a>
+            as a Front End Developer building responsive and{" "}
+            <a href="https://mealtrack.com/" target="_blank" rel="noopener">
+              highly configurable software
+            </a>{" "}
+            for a variety of clients.
+          </Typography>
+          <br />
+          <Typography gutterBottom variant="p">
+            Here are some technologies I work with everyday:{" "}
+          </Typography>
+        </motion.div>
+
         <Grid container>
           <Grid item xs={12} sm={6}>
-            <List>
-              <CustomListItem text="Javascript (jQuery, ES6)" />
-              <CustomListItem text="React" />
-              <CustomListItem text="Material UI" />
-            </List>
+            <motion.div
+              viewport={{ once: true }}
+              transition={{ delay: 2 }}
+              animate={{ x: [-100, 0], opacity: [0, 1] }}>
+              <List>
+                <CustomListItem text="Javascript (jQuery, ES6)" />
+                <CustomListItem text="React" />
+                <CustomListItem text="Material UI" />
+              </List>
+            </motion.div>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <List>
-              <CustomListItem text="Git / BitBucket" />
-              <CustomListItem text="SQL Server" />
-              <CustomListItem text="C# ASP.Net" />
-            </List>
+            <motion.div
+              viewport={{ once: true }}
+              transition={{ delay: 2 }}
+              animate={{ x: [100, 0], opacity: [0, 1] }}>
+              <List>
+                <CustomListItem text="Git / BitBucket" />
+                <CustomListItem text="SQL Server" />
+                <CustomListItem text="C# ASP.Net" />
+              </List>
+            </motion.div>
           </Grid>
         </Grid>
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          margin: "20px",
-        }}>
-        <img
+
+      <motion.div
+        viewport={{ once: true }}
+        transition={{ delay: 1, duration: 1.5 }}
+        animate={{ scale: [1, 1.1, 0.9, 1], opacity: [0, 1] }}>
+        <div
           style={{
-            borderRadius: "50%",
-            border: `2px solid ${PRIMARY_COLOR}`,
-            height: "250px",
-          }}
-          alt="My Headshot"
-          src="https://avatars.githubusercontent.com/u/34038688?v=4"
-        />
-      </div>
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            margin: "20px",
+          }}>
+          <img
+            style={{
+              borderRadius: "50%",
+              border: `2px solid ${PRIMARY_COLOR}`,
+              height: "250px",
+            }}
+            alt="My Headshot"
+            src="https://avatars.githubusercontent.com/u/34038688?v=4"
+          />
+        </div>
+      </motion.div>
     </Box>
   );
 };
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -2,28 +2,41 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
-
+import MotionButton from "../Components/MotionButton/MotionButton";
+import { motion } from "framer-motion";
 const Landing = () => {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" sx={{ ml: 10, mr: 10, mt: 5, mb: 20 }}>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", height: "100%" }}>
-        <Typography variant="h5" gutterBottom color="secondary">
-          Hi, my name is
-        </Typography>
-        <Typography variant="h3" style={{ fontSize: "60px" }} gutterBottom>
-          Bryan Borgesano.
-        </Typography>
-        <div style={{ maxWidth: "540px", paddingBottom: "25px" }}>
-          <Typography variant="p">
-            I'm a New York based Software Developer specializing in building responsive web based experiences.
+      <motion.div animate={{ y: [-200, 0], opacity: [0, 1] }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", height: "100%" }}>
+          <Typography variant="h5" gutterBottom color="secondary">
+            Hi, my name is
           </Typography>
+          <Typography variant="h3" style={{ fontSize: "60px" }} gutterBottom>
+            Bryan Borgesano.
+          </Typography>
+          <div style={{ maxWidth: "540px", paddingBottom: "25px" }}>
+            <Typography variant="p">
+              I'm a New York based Software Developer specializing in building responsive web based
+              experiences.
+            </Typography>
+          </div>
+          <motion.div
+            transition={{ delay: 0.5 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            animate={{ scale: [1.1, 0.9, 1] }}>
+            <MotionButton whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <a href="https://github.com/BryanBorge" target="_blank" rel="noopener">
+                <Button color="secondary" variant="outlined" style={{ width: "225px", height: "50px" }}>
+                  Check out my GitHub
+                </Button>
+              </a>
+            </MotionButton>
+          </motion.div>
         </div>
-        <a href="https://github.com/BryanBorge" target="_blank" rel="noopener">
-          <Button color="secondary" variant="outlined" style={{ width: "225px", height: "50px" }}>
-            Check out my GitHub
-          </Button>
-        </a>
-      </div>
+      </motion.div>
     </Box>
   );
 };

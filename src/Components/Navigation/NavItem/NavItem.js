965734486c721711a8da5491ae7eee2fb91ab2d1
Bryan Borgesano <BryanBorgesano97@gmail.com>
2022-04-03 15:37:27 -0400

    added hover animation

--- src/Components/Navigation/NavItem/NavItem.js
+++ src/Components/Navigation/NavItem/NavItem.js
@@ -6,7 +6,11 @@ import classes from "../Navigation.module.css";
 
 const NavItem = ({ text, onClick, selected }) => {
   return (
-    <motion.div className={classes.navItem} onClick={onClick} animate={{ opacity: selected ? 1 : 0.5 }}>
+    <motion.div
+      className={classes.navItem}
+      onClick={onClick}
+      animate={{ opacity: selected ? 1 : 0.5 }}
+      whileHover={{ scale: 1.1, y: -2 }}>
       <Box>
         <Typography>{text}</Typography>
         {selected && <motion.div className={classes.underline} layoutId="underline" />}
